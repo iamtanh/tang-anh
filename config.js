eb1a8a2cc29f5ec0493ae80685f0277312be9d72
iamtanh <86724676+iamtanh@users.noreply.github.com>
2021-06-30 20:32:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,15 @@
 const CONFIG = {
     introTitle: 'Chào anh, đồng chí!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Thanh xuân này vì một người lính mà khoác lên mình hai chữ Hậu phương`,
     btnIntro: 'Luv',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên �',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: '520 <33',
     btnNo: 'Không, em à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà. Yêu em nhiều nhiều 😘😘',
+    question:'Trên thế giới hơn 7 tỉ người mà sao mình lại gặp nhau nhỉ<3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Tránh cũng không được nha',
+    mess: 'Love you to the Moon and back',
     messDesc: 'Cảm ơn anh vì tất cả.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
